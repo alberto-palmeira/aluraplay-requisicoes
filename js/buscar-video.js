bcfe9b66e97c7626fa5cd089b85f7dfdc3adbf09
alberto-palmeira <jalberto.palmeira@gmail.com>
2023-01-13 12:35:05 -0300

    refatorado o código de busca para melhor organização e funcionalidade.

--- js/buscar-video.js
+++ js/buscar-video.js
@@ -4,24 +4,24 @@ import { conectaApi } from "./conecta-api.js";
 import constroiCard from "./mostrar-videos.js";
 
 const botaoPesquisa = document.querySelector('[data-botao-pesquisa]');
+const dadosDePesquisa = document.querySelector('[data-pesquisa]');
+const lista = document.querySelector('[data-lista]');
 
-botaoPesquisa.addEventListener('click', evento => buscarVideo(evento));
+botaoPesquisa.addEventListener('click', evento => {
+    evento.preventDefault();
+
+    buscarVideo()});
 
 // ---------- Funções ----------
 
-async function buscarVideo (evento) {
-    evento.preventDefault();
+async function buscarVideo () {
+    const busca = await conectaApi.buscaVideos(dadosDePesquisa.value);
     
-    const dadosDePesquisa = document.querySelector('[data-pesquisa]').value;
-    const busca = await conectaApi.buscaVideos(dadosDePesquisa);
-    
-    const lista = document.querySelector('[data-lista]');
+    if (busca.length > 0) {
+        lista.innerHTML = '';
 
-    lista.innerHTML = '';
-
-    busca.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
-
-    if(busca.length === 0) {
+        busca.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
+    } else {
         lista.innerHTML = `<h2 class="mensagem__titulo">Não existem vídeos com esse termo</h2>`
-    }
+    };
 };
